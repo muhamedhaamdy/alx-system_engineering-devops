3f1c52b619bb101e26818c1ab41be42ff38a693c
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-01-04 23:06:36 +0200

    "hotfix"

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # Basic matching
 string_to_match = ARGV[0]
-pattern = /^\d{10}/
+pattern = /^\[0-9]{10}$/
 match_data = string_to_match.scan(pattern).join
 puts "#{match_data}"
