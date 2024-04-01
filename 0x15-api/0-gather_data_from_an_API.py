9549f11472f15bec3580ab7ed6ad58d6ebcc72ca
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-01 03:32:11 +0200

    "hotfix"

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''returns information about his/her TODO list progress'''
+import requests
+import sys
+
+def get_info(resource, params = None):
+    '''get the the data from API'''
+    url = 'https://jsonplaceholder.typicode.com/' + resource
+    if params:
+        url += ('?' + params[0] + '=' + params[1])
+    req = requests.get(url)
+    return req.json()
+
+if __name__ == '__main__':
+    usr = get_info('users', ['id', sys.argv[1]])
+    todos = get_info('todos', ['userId', sys.argv[1]])
+    tasks_completed = [task for task in todos if task['completed']]
+    print('Employee {} is done with tasks({}/{}):'.format(usr[0]['name'],
+                                                          len(tasks_completed),
+                                                          len(todos)))
+    for task in tasks_completed:
+        print('\t {}'.format(task['title']))
