1719ce400549a48026f7ba3064e0b572b1218a03
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-08 02:02:36 +0200

    "hotfix"

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-'''returns information about his/her TODO list progress'''
+"""Request employee ID from API
+"""
+
 import csv
 import requests
-import sys
+from sys import argv
 
+if __name__ == "__main__":
 
-def get_info(resource, params=None):
-    '''get the the data from API'''
-    url = 'https://jsonplaceholder.typicode.com/'
+    def make_request(resource, param=None):
+        """Retrieve user from API
+        """
+        url = 'https://jsonplaceholder.typicode.com/'
         url += resource
         if param:
             url += ('?' + param[0] + '=' + param[1])
+
         r = requests.get(url)
+
         r = r.json()
         return r
 
-if __name__ == '__main__':
-    usr = get_info('users', ['id', sys.argv[1]])
-    todos = get_info('todos', ['userId', sys.argv[1]])
-    csv_file = '{}.csv'.format(sys.argv[1])
-    with open(csv_file, mode='w') as f:
+    user = make_request('users', ('id', argv[1]))[0]
+    tasks = make_request('todos', ('userId', argv[1]))
+
+    csv_filename = argv[1] + '.csv'
+    with open(csv_filename, mode='w') as f:
         writer = csv.writer(f,
                             delimiter=',',
                             quotechar='"',
                             quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([usr[0]['id'],
-                            usr[0]['username'],
+        for task in tasks:
+            writer.writerow([user['id'],
+                            user['username'],
                             task['completed'],
                             task['title']])
